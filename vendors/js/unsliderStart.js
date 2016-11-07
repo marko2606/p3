9f86eec10ac20d21cbca5911d545940e344c34bf
Marko Zelenbaba <zelenbaba.marko@gmail.com>
2016-11-07 03:01:05 +0100

    first commit

---
+++ vendors/js/unsliderStart.js
@@ -0,0 +1,21 @@
+jQuery(document).ready(function($) {
+			$('.my-slider').unslider({
+                dots: true,
+                autoplay: true,
+                arrows: {
+	//  Unslider default behaviour
+	prev: '<a class="unslider-arrow1 prev">Previous slide</a>',
+	next: '<a class="unslider-arrow1 next">Next slide</a>',
+
+
+}
+            });
+		});
+        
+        jQuery(document).ready(function($) {
+			$('.my-slider2').unslider({
+                dots: true,
+                autoplay: true,
+    
+            });
+		});
